70fcb0b016ee30426ef44faae2e65d89d3f0f4ad
wren <550375693@qq.com>
2021-11-14 22:02:08 +0800

    Changes

---
+++ pyqt5_study/QuitApplication.py
@@ -0,0 +1,42 @@
+# QDesktopWidget
+import sys
+from PyQt5.QtWidgets import QHBoxLayout, QMainWindow, QApplication, QPushButton, QWidget
+
+
+class QuitApplication(QMainWindow):
+    def __init__(self):
+        super(QuitApplication, self).__init__() # super().__init__()
+        # 设置主窗口的标题
+        self.setWindowTitle('退出按钮演示')
+        # 设置窗口的尺寸
+        self.resize(300, 120)
+
+        # 添加Button
+        self.button1 = QPushButton('退出程序')
+        # 将信号与槽关联
+        self.button1.clicked.connect(self.onClick_Button)
+
+        layout = QHBoxLayout()
+        layout.addWidget(self.button1)
+
+        mainFrame = QWidget()
+        mainFrame.setLayout(layout)
+        self.setCentralWidget(mainFrame)
+
+    # 按钮单击事件的方法（自定义的槽）
+    def onClick_Button(self):
+        sender = self.sender()
+        print(sender.text() + '按钮被按下')
+        app = QApplication.instance()
+
+        # 退出应用程序
+        app.quit()
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = QuitApplication()
+    main.show()
+    #app.exec_() # 运行主循环，并在退出时返回状态代码
+    sys.exit(app.exec_())
